e65fd00190660a0baa6374374eae36ffa7898250
Jan Vandepitte <Jan.Vandepitte@sirus.be>
2019-03-31 13:36:16 +0200

    init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,82 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate actix_web;
+
+use std::{env, io};
+
+use actix_files as fs;
+use actix_session::{CookieSession, Session};
+use actix_web::http::{header, Method, StatusCode};
+use actix_web::{
+    error, guard, middleware, web, App, Error, HttpRequest, HttpResponse, HttpServer,
+    Result,
+};
+use bytes::Bytes;
+use futures::unsync::mpsc;
+use futures::{future::ok, Future, Stream};
+
+fn main() -> io::Result<()> {
+    env::set_var("RUST_LOG", "actix_web=debug");
+    env_logger::init();
+    let sys = actix_rt::System::new("basic-example");
+
+    HttpServer::new(|| {
+        App::new()
+            // enable logger
+            .wrap(middleware::Logger::default())
+            // cookie session middleware
+            .wrap(CookieSession::signed(&[0; 32]).secure(false))
+            // register favicon
+            .service(favicon)
+            // register simple route, handle all methods
+            .service(welcome)
+            // with path parameters
+            .service(web::resource("/user/{name}").route(web::get().to(with_param)))
+            // async handler
+            .service(
+                web::resource("/async/{name}").route(web::get().to_async(index_async)),
+            )
+            // async handler
+            .service(
+                web::resource("/async-body/{name}")
+                    .route(web::get().to(index_async_body)),
+            )
+            .service(
+                web::resource("/test").to(|req: HttpRequest| match *req.method() {
+                    Method::GET => HttpResponse::Ok(),
+                    Method::POST => HttpResponse::MethodNotAllowed(),
+                    _ => HttpResponse::NotFound(),
+                }),
+            )
+            .service(web::resource("/error").to(|| {
+                error::InternalError::new(
+                    io::Error::new(io::ErrorKind::Other, "test"),
+                    StatusCode::INTERNAL_SERVER_ERROR,
+                )
+            }))
+            // static files
+            .service(fs::Files::new("/static", "static").show_files_listing())
+            // redirect
+            .service(web::resource("/").route(web::get().to(|req: HttpRequest| {
+                println!("{:?}", req);
+                HttpResponse::Found()
+                    .header(header::LOCATION, "static/welcome.html")
+                    .finish()
+            })))
+            // default
+            .default_resource(|r| {
+                // 404 for GET request
+                r.route(web::get().to(p404))
+                    // all requests that are not `GET`
+                    .route(
+                        web::route()
+                            .guard(guard::Not(guard::Get()))
+                            .to(|| HttpResponse::MethodNotAllowed()),
+                    )
+            })
+    })
+    .bind("127.0.0.1:8080")?
+    .start();
+
+    println!("Starting http server: 127.0.0.1:8080");
+    sys.run()
 }
